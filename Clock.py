8600488bc164dd86bf555a8ab51c0d75d72814b1
Obeth297 <142341592+Obeth297@users.noreply.github.com>
2024-10-31 00:54:24 -0400

    Made the clock class
    I made the actual usable code that saves a punch in time and returns how long the session was.

--- Clock.py
+++ Clock.py
@@ -1,10 +1,25 @@
 from datetime import datetime
+
 class Clock:
     """A class that will allow the user to punch in and out and record the time spent."""
-    def punch_in():
-        start_time = datetime.now()
-        return start_time
+    def __init__(self):
+        self.punch_in_time = None
+        self.punch_out_time = None
 
-    def punch_out():
-        end_time = datetime.now()
-        return None
+    def punch_in(self):
+        if self.punch_in_time != None:
+            print("Already punched in at:", self.punch_in_time)
+        else:
+            self.punch_in_time = datetime.now().replace(microsecond=0)
+            print("Punched in at:", self.punch_in_time)
+
+    def punch_out(self):
+        if self.punch_in_time == None:
+            print("You need to punch in first!")
+        elif self.punch_out_time != None:
+            print("Already punched out at:", self.punch_out_time)
+        else:
+            self.punch_out_time = datetime.now().replace(microsecond=0)
+            session_time = self.punch_out_time - self.punch_in_time
+            return session_time
+    
