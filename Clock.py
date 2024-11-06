1f17df99fa68e6454d604aa4d9c3a4046539426f
Obeth297 <142341592+Obeth297@users.noreply.github.com>
2024-11-05 20:13:37 -0500

    Changed the names o fthe punch_in and punch_ out methods to clock_in and clock_out.

--- Clock.py
+++ Clock.py
@@ -3,23 +3,23 @@ from datetime import datetime
 class Clock:
     """A class that will allow the user to punch in and out and record the time spent."""
     def __init__(self):
-        self.punch_in_time = None
-        self.punch_out_time = None
+        self.clock_in_time = None
+        self.clock_out_time = None
 
-    def punch_in(self):
-        if self.punch_in_time != None:
-            print("Already punched in at:", self.punch_in_time)
+    def clock_in(self):
+        if self.clock_in_time != None:
+            print("Already punched in at:", self.clock_in_time)
         else:
-            self.punch_in_time = datetime.now().replace(microsecond=0)
-            print("Punched in at:", self.punch_in_time)
+            self.clock_in_time = datetime.now().replace(microsecond=0)
+            return self.clock_in_time
 
-    def punch_out(self):
-        if self.punch_in_time == None:
+    def clock_out(self):
+        if self.clock_in_time == None:
             print("You need to punch in first!")
-        elif self.punch_out_time != None:
-            print("Already punched out at:", self.punch_out_time)
+        elif self.clock_out_time != None:
+            print("Already punched out at:", self.clock_out_time)
         else:
-            self.punch_out_time = datetime.now().replace(microsecond=0)
-            session_time = self.punch_out_time - self.punch_in_time
+            self.clock_out_time = datetime.now().replace(microsecond=0)
+            session_time = self.clock_out_time - self.clock_in_time
             return session_time
     
