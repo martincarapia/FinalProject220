c8214e3b117f17011fc8173e80ae4a73e8cefcc4
Martin C <78377730+MilkManMaki@users.noreply.github.com>
2024-11-16 15:08:56 -0500

    docs: updating files to follow standard comment coding

--- User.py
+++ User.py
@@ -1,14 +1,30 @@
+from Clock import Clock
+
 class User:
-    def __init__(self, id: int, username: str, password: str, timecard: object, last_action: str, is_admin: bool):
-        """Initiate info for user with also a timecard object"""
+    """
+    A class to represent a user with an associated timecard object.
+    """
+
+    def __init__(self, id: int, username: str, password: str):
+        """
+        Initialize the User instance with an ID, username, password, and a timecard object.
+
+        Args:
+            id (int): The unique identifier for the user.
+            username (str): The username of the user.
+            password (str): The password of the user.
+        """
         self.id = id
         self.username = username
         self.password = password
-        # Will hold the timecard class to get total hours and when clocked
-        self.timecard = timecard
-        # Last two attributes may not be needed
-        self.last_action = last_action
-        self.is_admin = is_admin
-    
+        # Will hold the Clock class instance to get total hours and when clocked
+        self.clock = Clock()
+
     def __str__(self):
-        return f"ID: {self.id}, Username: {self.username}, Password: {self.password}, Total hours: {self.timecard.total_hours}"
+        """
+        Return a string representation of the User instance.
+
+        Returns:
+            str: A string containing the user's ID, username, and password.
+        """
+        return f"ID: {self.id}, Username: {self.username}, Password: {self.password}"
